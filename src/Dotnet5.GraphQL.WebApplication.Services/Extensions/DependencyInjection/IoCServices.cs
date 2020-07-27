b54fc407b1594dd44453813e7cb6f64617b84304
Antônio Falcão <arfj@edu.univali.br>
2020-07-27 18:15:35 -0300

    Adding AutoMapper

--- src/Dotnet5.GraphQL.WebApplication.Services/Extensions/DependencyInjection/IoCServices.cs
+++ src/Dotnet5.GraphQL.WebApplication.Services/Extensions/DependencyInjection/IoCServices.cs
@@ -1,3 +1,5 @@
+using AutoMapper;
+using Dotnet5.GraphQL.WebApplication.Services.Profiles;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Dotnet5.GraphQL.WebApplication.Services.Extensions.DependencyInjection
@@ -5,7 +7,7 @@ namespace Dotnet5.GraphQL.WebApplication.Services.Extensions.DependencyInjection
     public static class IoCServices
     {
         public static IServiceCollection AddAutoMapper(this IServiceCollection services)
-            => services.AddAutoMapper();
+            => services.AddAutoMapper(typeof(ModelToDomainProfile));
 
         public static IServiceCollection AddServices(this IServiceCollection services)
             => services.AddScoped<IProductService, ProductService>();
