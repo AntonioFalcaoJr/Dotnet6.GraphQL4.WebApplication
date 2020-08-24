42fb8d47d6915eba1e4773a707e5ee7514f6f1dc
antoniofalcao <arfj@edu.univali.br>
2020-08-24 00:47:57 -0300

    Launch message on mutation

--- src/Dotnet5.GraphQL.Store.WebAPI/GraphQL/StoreMutation.cs
+++ src/Dotnet5.GraphQL.Store.WebAPI/GraphQL/StoreMutation.cs
@@ -1,4 +1,5 @@
 using Dotnet5.GraphQL.Store.Services;
+using Dotnet5.GraphQL.Store.Services.Messages;
 using Dotnet5.GraphQL.Store.Services.Models;
 using Dotnet5.GraphQL.Store.WebAPI.GraphQL.Types.Reviews;
 using GraphQL.Types;
@@ -7,15 +8,17 @@ namespace Dotnet5.GraphQL.Store.WebAPI.GraphQL
 {
     public class StoreMutation : ObjectGraphType
     {
-        public StoreMutation(IProductService service)
+        public StoreMutation(IProductService productService, IReviewMessageService messageService)
         {
             FieldAsync<ReviewGraphType>("createReview",
                 arguments: new QueryArguments(new QueryArgument<NonNullGraphType<ReviewInputGraphType>> {Name = "review"}),
                 resolve: async context =>
                 {
-                    var review = context.GetArgument<ReviewModel>("review");
-                    return await context.TryAsyncResolve(async fieldContext
-                        => await service.AddReviewAsync(review, context.CancellationToken));
+                    var model = context.GetArgument<ReviewModel>("review");
+                    var review = await context.TryAsyncResolve(async fieldContext
+                        => await productService.AddReviewAsync(model, context.CancellationToken));
+                    messageService.Add(model);
+                    return review;
                 });
         }
     }
