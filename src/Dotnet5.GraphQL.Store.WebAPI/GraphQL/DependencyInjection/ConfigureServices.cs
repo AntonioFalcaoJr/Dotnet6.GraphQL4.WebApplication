505c73cd1966ee28a612c85f4229c2c3fc3c9d82
antoniofalcao <arfj@edu.univali.br>
2020-08-25 00:08:10 -0300

    Changing mutation to use service provider to resolve dependencies

--- src/Dotnet5.GraphQL.Store.WebAPI/GraphQL/DependencyInjection/ConfigureServices.cs
+++ src/Dotnet5.GraphQL.Store.WebAPI/GraphQL/DependencyInjection/ConfigureServices.cs
@@ -2,7 +2,6 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using GraphQL.Server;
-using GraphQL.Types;
 
 namespace Dotnet5.GraphQL.Store.WebAPI.GraphQL.DependencyInjection
 {
--- src/Dotnet5.GraphQL.Store.WebAPI/GraphQL/StoreMutation.cs
+++ src/Dotnet5.GraphQL.Store.WebAPI/GraphQL/StoreMutation.cs
@@ -1,15 +1,17 @@
+using System;
 using Dotnet5.GraphQL.Store.Services;
 using Dotnet5.GraphQL.Store.Services.Messages;
 using Dotnet5.GraphQL.Store.Services.Models;
 using Dotnet5.GraphQL.Store.WebAPI.GraphQL.Types.Reviews;
 using GraphQL;
 using GraphQL.Types;
+using GraphQL.Utilities;
 
 namespace Dotnet5.GraphQL.Store.WebAPI.GraphQL
 {
     public class StoreMutation : ObjectGraphType
     {
-        public StoreMutation(IProductService productService, IReviewMessageService messageService)
+        public StoreMutation(IServiceProvider provider)
         {
             FieldAsync<ReviewGraphType>("createReview",
                 arguments: new QueryArguments(new QueryArgument<NonNullGraphType<ReviewInputGraphType>> {Name = "review"}),
@@ -17,8 +19,9 @@ namespace Dotnet5.GraphQL.Store.WebAPI.GraphQL
                 {
                     var model = context.GetArgument<ReviewModel>("review");
                     var review = await context.TryAsyncResolve(async fieldContext
-                        => await productService.AddReviewAsync(model, context.CancellationToken));
-                    messageService.Add(model);
+                        => await provider.GetRequiredService<IProductService>()
+                            .AddReviewAsync(model, context.CancellationToken));
+                    provider.GetRequiredService<IReviewMessageService>().Add(model);
                     return review;
                 });
         }
