908ea513389a1103b158552b3f9e4fc65f358faa
antoniofalcao <arfj@edu.univali.br>
2020-09-09 17:45:39 -0300

    Using Scrutor to register dependencies

---
+++ src/Dotnet5.GraphQL.Store.Services.Abstractions/DependencyInjection/ConfigureService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Scrutor;
+
+namespace Dotnet5.GraphQL.Store.Services.Abstractions.DependencyInjection
+{
+    public static class ConfigureService
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services)
+            => services.Scan(selector
+                => selector
+                    .FromCallingAssembly()
+                    .AddClasses(filter => filter.AssignableToAny(typeof(IService<,,>)))
+                    .UsingRegistrationStrategy(RegistrationStrategy.Skip)
+                    .AsImplementedInterfaces()
+                    .WithScopedLifetime());
+    }
+}
