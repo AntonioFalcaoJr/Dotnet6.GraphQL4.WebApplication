6e676354403eb0e8d585d2b9fc6758981f914eab
Antônio Falcão <arfj@edu.univali.br>
2020-10-27 23:46:08 -0300

    Adding PageParams and respective Graph type

---
+++ src/Dotnet5.GraphQL3.Repositories.Abstractions/Pages/PageParams.cs
@@ -0,0 +1,23 @@
+﻿namespace Dotnet5.GraphQL3.Repositories.Abstractions.Pages
+{
+    public class PageParams
+    {
+        private const int DefaultSize = 10;
+        private const int DefaultIndex = 1;
+        
+        private int _index;
+        private int _size;
+
+        public int Size
+        {
+            get => _size <= 0 ? DefaultSize : _size;
+            set => _size = value;
+        }
+
+        public int Index
+        {
+            get => _index <= 0 ? DefaultIndex : _index;
+            set => _index = value;
+        }
+    }
+}
