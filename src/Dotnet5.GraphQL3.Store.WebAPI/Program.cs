f1a509e92f5c10101a5afe1e28fb7c1f3a57feff
Antônio Falcão Jr <antonio.falcao@xpi.com.br>
2021-02-09 20:50:04 -0300

    default service provider

--- src/Dotnet5.GraphQL3.Store.WebAPI/Program.cs
+++ src/Dotnet5.GraphQL3.Store.WebAPI/Program.cs
@@ -7,8 +7,12 @@ namespace Dotnet5.GraphQL3.Store.WebAPI
     {
         private static IHostBuilder CreateHostBuilder(string[] args)
             => Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder
-                    => webBuilder.UseStartup<Startup>());
+                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>())
+                .UseDefaultServiceProvider((context, options) =>
+                    {
+                        options.ValidateScopes = context.HostingEnvironment.IsDevelopment();
+                        options.ValidateOnBuild = true;
+                    });
 
         public static void Main(string[] args)
             => CreateHostBuilder(args).Build().Run();
